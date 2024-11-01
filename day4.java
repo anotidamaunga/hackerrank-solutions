7a9f146e21edfa3f3b485982043d05aef8a1bdce
anotidamaunga <ax1tfg@gmail.com>
2024-11-01 22:14:15 +0100

    day3 code

--- day4.java
+++ day4.java
@@ -32,10 +32,17 @@ public class day4{
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         for (int i = 0; i < T; i++) {
-            int age = sc.nextInt();
+            int age = sc.nextInt();Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
 
             System.out.println();
         }
-        sc.close();
+
     }
-}
+
